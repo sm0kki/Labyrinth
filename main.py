d1f648935d7dbb3b7a968a2198a881c0fb04c3e4
george <abonent-21@yandex.ru>
2023-01-08 18:27:11 +0300

    Add enemy and movement of enemy

--- main.py
+++ main.py
@@ -7,11 +7,8 @@ import os
 # margin for upper left corner
 TOP = 0
 LEFT = 0
-CELL_SIZE = 200
+CELL_SIZE = 100
 SIZE = WIDTH, HEIGHT = 800, 800
-
-MAX_X = 42
-MAX_Y = 11
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -37,13 +34,17 @@ class Labyrinth:
     # создание карты
     def __init__(self, file_name):
         self.map = []
+        self.height = 0
         with open(file_name) as file:
             for line in file:
                 self.map.append(list(line))
+                self.height += 1
+        self.width = len(self.map[0])
         self.left = LEFT
         self.top = TOP
         self.cell_size = CELL_SIZE
         self.pos_of_hero = None
+        self.pos_of_enemy = None
 
     def render(self, scr):
         y = self.left
@@ -52,13 +53,39 @@ class Labyrinth:
             for j in range(len(self.map[i])):
                 if self.map[i][j] == '#':
                     Tile('wall', j, i)
-                if self.map[i][j] == '.':
+                elif self.map[i][j] == '.':
                     Tile('empty', j, i)
-                if self.map[i][j] == '@':
+                elif self.map[i][j] == '@':
                     Tile('empty', j, i)
                     self.pos_of_hero = (j, i)
+                elif self.map[i][j] == '!':
+                    Tile('empty', j, i)
+                    self.pos_of_enemy = (j, i)
                 x += self.cell_size
             y += self.cell_size
+
+    def find_path_step(self, start, target):
+        inf = 1000
+        x, y = start
+        distance = [[inf] * self.width for _ in range(self.height)]
+        distance[y][x] = 0
+        prev = [[None] * self.width for _ in range(self.height)]
+        queue = [(x, y)]
+        while queue:
+            x, y = queue.pop(0)
+            for dx, dy in (1, 0), (0, 1), (-1, 0), (0, -1):
+                next_x, next_y = x + dx, y + dx
+                if 0 <= next_x < self.width and 0 < next_y < self.height and \
+                        self.map[next_y][next_x] == '.' and distance[next_y][next_x] == inf:
+                    distance[next_y][next_x] = distance[y][x] + 1
+                    prev[next_y][next_x] = (x, y)
+                    queue.append((next_x, next_y))
+        x, y = target
+        if distance[y][x] == inf or start == target:
+            return start
+        while prev[y][x] != start:
+            x, y = prev[y][x]
+        return x, y
 
 
 class Tile(pygame.sprite.Sprite):
@@ -77,16 +104,26 @@ class Player(pygame.sprite.Sprite):
         self.pos = (pos_x, pos_y)
 
     def move(self, x, y):
-        self.pos = (x, y)  # for table
-        camera.dx -= CELL_SIZE * (x - self.pos[0])
-        camera.dy -= CELL_SIZE * (y - self.pos[1])
-        self.pos = (x, y)
-        for sprite in all_sprites:
-            camera.apply(sprite)
+        self.pos = (x, y) # for table
+
 
     def render(self, pos_x, pos_y):
         self.rect = self.image.get_rect().move(CELL_SIZE * pos_x, CELL_SIZE * pos_y)
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(enemy_group, all_sprites)
+        self.image = enemy_image
+        self.rect = self.image.get_rect().move(CELL_SIZE * pos_x, CELL_SIZE * pos_y)
         self.pos = (pos_x, pos_y)
+
+    def move(self, x, y):
+        next_position = self.labyrinth.find_path_step(*self.pos, *self.labyrinth.pos_of_hero)
+
+
+    def render(self, pos_x, pos_y):
+        self.rect = self.image.get_rect().move(CELL_SIZE * pos_x, CELL_SIZE * pos_y)
 
 
 class Game:
@@ -94,10 +131,12 @@ class Game:
         self.labyrinth = Labyrinth(map_for_labyrinth)
         self.labyrinth.render(screen)
         self.player = Player(*self.labyrinth.pos_of_hero)
+        self.enemy = Enemy(*self.labyrinth.pos_of_enemy)
         self.pos = self.labyrinth.pos_of_hero
 
     def render(self, screen1):
         self.player.render(*self.player.pos)
+        self.enemy.render(*self.enemy.pos)
         self.labyrinth.render(screen1)
 
     def move(self, hero, movement):
@@ -126,6 +165,7 @@ class Game:
                 self.labyrinth.map[y][x + 1] = '@'
 
 
+
 class Camera:
     def __init__(self):
         self.dx = 0
@@ -149,11 +189,15 @@ if __name__ == '__main__':
     all_sprites = pygame.sprite.Group()
     hero_group = pygame.sprite.Group()
     tiles_group = pygame.sprite.Group()
+    enemy_group = pygame.sprite.Group()
 
     tile_images = {'wall': pygame.transform.scale(load_image('texture\\box1.png'), (CELL_SIZE, CELL_SIZE)),
                    'empty': pygame.transform.scale(load_image('texture\\grass.png'), (CELL_SIZE, CELL_SIZE))}
     player_image = load_image('skin/Pink_Monster.png')
     player_image = pygame.transform.scale(player_image, (CELL_SIZE, CELL_SIZE))
+
+    enemy_image = load_image('skin/Black_Monster.png')
+    enemy_image = pygame.transform.scale(enemy_image, (CELL_SIZE, CELL_SIZE))
 
     game = Game("maps/map_1.txt")
     game.render(screen)
@@ -165,7 +209,7 @@ if __name__ == '__main__':
     fps = 60
 
     while running:
-        pygame.time.delay(100)
+        pygame.time.delay(40)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -191,6 +235,7 @@ if __name__ == '__main__':
 
         tiles_group.draw(screen)
         hero_group.draw(screen)
+        enemy_group.draw(screen)
         game.render(screen)
 
         pygame.display.flip()
